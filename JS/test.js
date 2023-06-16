3ef641b7d3d9c6d7e68f7648e8ceff1d62688923
Samuel Lutumba <lutumbasamuel9@gmail.com>
2023-06-16 03:27:12 +0200

    Adding questions to the quiz

--- JS/test.js
+++ JS/test.js
@@ -1,19 +1,76 @@
 // Define the quiz data
 var quizData = [
   {
-    question: 'What is the capital of France?',
-    options: ['Paris', 'London', 'Berlin', 'Madrid'],
-    answer: 'Paris'
+    question: 'What is AI?',
+    options: ['Animal Informtion', 'Artifitial Intelligance', 'Brand', 'Artifitial Information'],
+    answer: 'Artifitial Intelligance',
+    type: 'radio'
   },
   {
-    question: 'What is the currency of Japan?',
-    options: ['Yen', 'Dollar', 'Euro', 'Pound'],
-    answer: 'Yen',
+    question: 'What is an Autonomous Vehicle?',
+    options: ['A self driving car', 'A self caring car', 'A self maintaining car', 'An auto repairing car'],
+    answer: 'A self driving car',
+    type: 'radio'
   },
   {
-    question: 'What is the largest planet in our solar system?',
-    options: ['Jupiter', 'Saturn', 'Neptune', 'Uranus'],
-    answer: 'Jupiter',
+    question: 'Which of these is NOT a phone brand',
+    options: ['Apple', 'Russels', 'LG', 'Samsung'],
+    answer: 'Russels',
+    type: 'radio'
+  },
+  {
+    question: 'What is a smart home?',
+    options: ['A house with a lot of books in it', 'A house equipped technology', 'A house that thinks on its own', 'A house that knows its owners'],
+    answer: 'A house equipped technology',
+    type: 'radio'
+  },
+  {
+    question: 'Which of these is NOT a laptop brand?',
+    options: ['Apple', 'Dell', 'Mazda', 'Samsung'],
+    answer: 'Mazda',
+    type: 'radio'
+  },
+  {
+    question: 'Which of these is NOT a phone brand?',
+    options: ['Apple', 'Philips', 'LG', 'Samsung'],
+    answer: 'Philips',
+    type: 'radio'
+  },
+  {
+    question: 'which of these is hardware?',
+    options: ['Windows', 'IOS', 'Microsoft Office', 'Monitor'],
+    answer: 'Monitor',
+    type: 'radio'
+  },
+  {
+    question: 'which of these is software?',
+    options: ['Android', 'RAM', 'ROM', 'CPU'],
+    answer: 'Android',
+    type: 'radio'
+  },
+  {
+    question: 'What is regarded as the brain of the computer?',
+    options: ['Brain', 'RAM', 'HDD', 'CPU'],
+    answer: 'CPU',
+    type: 'radio'
+  },
+  {
+    question: 'WHat does RAM stand for?',
+    options: ['Read After Me', 'Random Access Memory', 'Read And Memorise', 'I dont Know'],
+    answer: 'Random Access Memory',
+    type: 'radio'
+  },
+  {
+    question: 'which of these is an example of an Operating System?',
+    options: ['Samsung', 'Apple', 'Doors', 'Ubuntu'],
+    answer: 'Android',
+    type: 'radio'
+  },
+  {
+    question: 'what does USB stand for?',
+    options: ['Universal Serial Bus', 'Universl Series Bus', 'Universal Series Buses', 'Use Safely Before'],
+    answer: 'Android',
+    type: 'radio'
   }
 ];
 
